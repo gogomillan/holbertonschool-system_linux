f9d18ceded12408c4c8fe0ed0b167829acf36b64
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-07-17 00:50:16 +0000

    Relocating files to the right dir

---
+++ 0x00-ls/hls.h
@@ -0,0 +1,27 @@
+#ifndef GOGOMILLAN
+#define GOGOMILLAN OK
+
+#define TRUE  1
+#define FALSE 0
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <dirent.h>
+
+int _readdir(char *dir_name);
+extern int errno;
+/**
+ * dirent - struct for directory entry
+ *
+ * @d_ino: inode number
+ * @d_off: not an offset; see NOTES
+ * @d_reclen: length of this record
+ * @d_type: type of file; not supported by all filesystem types
+ * @d_name[256]: filename
+ */
+struct dirent *read;
+
+#endif /* GOGOMILLAN */
