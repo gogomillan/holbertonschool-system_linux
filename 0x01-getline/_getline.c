9151274c72a33ec09d957c949ae8562468ad27be
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-07-29 05:01:18 +0000

    Task1: _getline

---
+++ 0x01-getline/_getline.c
@@ -0,0 +1,12 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#include "_getline.h"
+
+char *_getline(const int fd)
+{
+	(void)fd;
+	return (NULL);
+}
---
+++ 0x01-getline/_getline.h
@@ -0,0 +1,8 @@
+#ifndef GOGOMILLAN
+#define GOGOMILLAN
+
+#define READ_SIZE 512
+
+char *_getline(const int fd);
+
+#endif
