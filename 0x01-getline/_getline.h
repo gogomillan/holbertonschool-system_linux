b6d7cd002bcfbe276e8c2b6235802af0899ad138
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-07-30 17:56:52 +0000

    Task2: Adding the structure to the header file

--- 0x01-getline/_getline.h
+++ 0x01-getline/_getline.h
@@ -9,4 +9,15 @@ char *_getline(const int fd);
 char *_strchr(char *s, int c);
 size_t _strlen(char *s);
 
+/**
+ * struct listfd_s - singly linked list for fd
+ * @fd: The file descriptor associated
+ * @bufferst: The buffer static associated to fd
+ */
+typedef struct listfd_s
+{
+	int fd;
+	char *bufferst[BUFF_SIZE];
+} listfd_t;
+
 #endif
