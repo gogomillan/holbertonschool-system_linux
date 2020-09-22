08b8496fba12ddcdabf4df2ed5d6b5403b2587cf
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-09-22 05:42:03 +0000

    Task 7: puts

--- 0x05-libasm/7-puts.asm
+++ 0x05-libasm/7-puts.asm
@@ -1,36 +1,36 @@
-BITS 64
+; NASM code style
+; Prototype when used in C: size_t asm_puts(const char *str)
 
-section .data
+	bits 64
 
-str_c dq 0
+	section .data
+	str_c dq 0			; Variable to store the arg1
 
-section .bss
+	section .text		; Code section
+	global asm_puts		; Exporting the function name
+	extern asm_strlen	; Import the function using the symbol
 
-section .text
-        global asm_puts
-	extern asm_strlen
-
-; string length funciton
 asm_puts:
-	push rbp                ; Setup stack frame
-	mov rbp, rsp            ; Those two lines are equivalent to 'enter'
+	push rbp			; Setup stack frame
+	mov rbp, rsp		; Those two lines are equivalent to 'enter'
 
-	push rbx		; save rbx value
+	push rbx			; save rbx value
 	mov [str_c], rdi	; store the arg1 value
 
-	mov rax, 0		; set 0 the return value
+	mov rax, 0			; set 0 the return value
 	call asm_strlen		; call the function asm_strlen
 	mov rbx, rax		; save the return value
 
-	mov rax, 1              ; Write syscall
-	mov rdi, 1              ; write to stdout
-	mov rsi, [str_c]        ; Address of the character to be printed (Address of the stack)
+	mov rax, 1			; Write syscall
+	mov rdi, 1			; write to stdout
+	mov rsi, [str_c]	; Address of the character to be printed (Address of the stack)
 	mov rdx, rbx		; Write strlen bytes
-	syscall                 ; call the syscall
+	syscall				; call the syscall (Write)
 
-	mov rax, rbx            ; return value printd
+	mov rax, rbx		; return value printed
 
-	pop rbx			; set back the previos value
-	mov rsp, rbp            ; Restore previous stack frame
-	pop rbp                 ; Those two lines are equivalent to 'leave'
-        ret			; return
+	pop rbx				; set back the previos value
+	mov rsp, rbp		; Restore previous stack frame
+	pop rbp				; Those two lines are equivalent to 'leave'
+
+	ret					; return
