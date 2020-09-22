1c7c1dd85cb426a1cddfb23328d0254dd131099e
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-09-22 06:25:27 +0000

    Task 8: strcasecmp

--- 0x05-libasm/8-strcasecmp.asm
+++ 0x05-libasm/8-strcasecmp.asm
@@ -1,28 +1,27 @@
-BITS 64
+; NASM code style
+; Prototype when used in C: int asm_strcasecmp(const char *s1, const char *s2)
 
-section .data
+	bits 64
 
-section .bss
+	section .text			; Code section
+	global asm_strcasecmp	; Exporting the function name
 
-section .text
-        global asm_strcasecmp
-
-; string compare funciton
 asm_strcasecmp:
-        push rbp                	; prologue
-        mov rbp, rsp
-	push rdx			; save rdx value in stack
-	push r11			; save r11 value in stack
-	push r12			; save r12 value in stack
+	push rbp				; prologue
+	mov rbp, rsp
+	push rdx				; save rdx value in stack
+	push r11				; save r11 value in stack
+	push r12				; save r12 value in stack
 
-	mov rdx, 0			; start the iterator in 0
-	mov r11, 0			; start the registeer compator 0
-	mov r12, 0			; start the registeer compator 0
-count_c:                        	; for statement
-	inc rdx				; increment the iterator
+	mov rdx, 0				; start the iterator in 0
+	mov r11, 0				; start the registeer compator 0
+	mov r12, 0				; start the registeer compator 0
+
+count_c:					; for statement
+	inc rdx					; increment the iterator
 
 	mov r12b, byte [rsi + rdx - 1]	; check if the char in arg1
-	mov r11b, byte [rdi + rdx - 1]  ; check if the char in arg2
+	mov r11b, byte [rdi + rdx - 1] 	; check if the char in arg2
 
 	cmp r12b, 65			; convert from uppercase to lowercase
 	jl next_count1			; if the byte is in uppercase
@@ -36,29 +35,31 @@ next_count1:
 	jl conv_lower_arg2
 
 next_count2:
-        cmp r12b, 0x00                  ; is not a null char
-        je next_c                       ; if it is go out of for loop
-        cmp r11b, 0x00                  ; is not a null char
-        je next_c                       ; if it is go out of for loop
+	cmp r12b, 0x00			; is not a null char
+	je end					; if it is go out of for loop
+	cmp r11b, 0x00			; is not a null char
+	je end					; if it is go out of for loop
 
-        cmp r11b, r12b			; check if char in arg1 is eql to char in arg2
-        je count_c			; if it is, continue in the for loop
-next_c:					; end lopp
-	mov rax, 0			; set the return value in 0
+	cmp r11b, r12b			; check if char in arg1 is eql to char in arg2
+	je count_c				; if it is, continue in the for loop
+
+end:						; end lopp
+	mov rax, 0				; set the return value in 0
 	mov rax, r11			; compare the previous variables
 	sub rax, r12
 
 	pop r12
-	pop r11				; set back the previous value of r8
-	pop rdx				; set back the previous value of rdx
-        mov rsp, rbp            	; epilogue
-        pop rbp				; set back the previous value of rbp
-        ret                     	; return the len value
+	pop r11					; set back the previous value of r8
+	pop rdx					; set back the previous value of rdx
+	mov rsp, rbp			; epilogue
+	pop rbp					; set back the previous value of rbp
 
-conv_lower_arg1:			; convert from uppercase to lowercase
+	ret						; return the len value
+
+conv_lower_arg1:			; symbol: function that converts from uppercase to lowercase
 	add r12b, 32
 	jmp next_count1
 
-conv_lower_arg2:			; convert from uppercase to lowercase
+conv_lower_arg2:			; symbol: function that converts from uppercase to lowercase
 	add r11b, 32
-        jmp next_count2
+	jmp next_count2
