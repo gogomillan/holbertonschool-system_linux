f036161e546004a06e0d851df106f901d4a336ff
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-09-22 22:19:31 +0000

    Task 11: strcspn

--- 0x05-libasm/11-strcspn.asm
+++ 0x05-libasm/11-strcspn.asm
@@ -1,50 +1,50 @@
-BITS 64
+; NASM code style
+; Prototype when used in C: size_t asm_strcspn(const char *s, const char *reject)
 
-section .data
+	bits 64
 
-section .bss
+	section .text				; Code section
+	global asm_strcspn			; Exporting the function name
 
-section .text
-	global asm_strcspn
-
-; string length funciton
 asm_strcspn:
-	push rbp		; prologue
+	push rbp					; prologue
 	mov rbp, rsp
 
-	push rbx		; save rbx value in stack
-	push rcx		; save rcx value in stack
-	push r11		; save r11 value in stack
+	push rbx					; save rbx value in stack
+	push rcx					; save rcx value in stack
+	push r11					; save r11 value in stack
 
-	mov rcx, 0		; start in 0 the rcx register
-	mov r11, 0		; start in 0 the r11 register
+	mov rcx, 0					; start in 0 the rcx register
+	mov r11, 0					; start in 0 the r11 register
 
-count_c1:				; for statement
+count_c1:						; for statement
 	cmp byte [rdi + rcx], 0x00	; compare each character of arg with caracter null
-	je end_c			; if the char in arg is not = to caracter null continue in for
+	je end						; if the char in arg is not = to caracter null continue in for
 
-	mov rbx, 0			; set the iterator in 0
-count_c2:				; second for
+	mov rbx, 0					; set the iterator in 0
+
+count_c2:						; second for
 	mov r11b, [rsi + rbx]		; if accept byte is null
-	cmp r11b, 0x00			; continue the first loop
-	je count_c3			; continue the first loop
+	cmp r11b, 0x00				; continue the first loop
+	je count_c3					; continue the first loop
 
-	inc rbx				; else increase the iterator
+	inc rbx						; else increase the iterator
 	cmp r11b, [rdi + rcx]		; if accept byte != to the byte in arg1
-	je end_c			; break both loop and end program
-	jmp count_c2			; continue the 2nd loop
+	je end						; break both loop and end program
+	jmp count_c2				; continue the 2nd loop
 
 count_c3:
-	inc rcx				; else increment the length of arg1
-	jmp count_c1			; continue the for
+	inc rcx						; else increment the length of arg1
+	jmp count_c1				; continue the for
 
-end_c:
-	mov rax, rcx		; set the len in the return value
+end:
+	mov rax, rcx				; set the len in the return value
 
-	pop r11			; set the r11 register with its old value
-	pop rcx			; set the rcx register with its old value
-	pop rbx			; set the rbx register with its old value
+	pop r11						; set the r11 register with its old value
+	pop rcx						; set the rcx register with its old value
+	pop rbx						; set the rbx register with its old value
 
-	mov rsp, rbp		; epilogue
+	mov rsp, rbp				; epilogue
 	pop rbp
-	ret			; return the len value
+
+	ret							; return the len value
