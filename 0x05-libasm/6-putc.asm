203b819793a989ffb1bf67a5e28c1d5c1c3249d7
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-09-22 05:11:49 +0000

    Task 6: putc

--- 0x05-libasm/6-putc.asm
+++ 0x05-libasm/6-putc.asm
@@ -1,27 +1,27 @@
-BITS 64
+; NASM code style
+; Prototype when used in C: size_t asm_putc(int c)
 
-section .data
+	bits 64
 
-section .bss
+	section .text	; Code section
+	global asm_putc	; Exporting the function name
 
-section .text
-        global asm_putc
-
-; string length funciton
 asm_putc:
-        push rbp		; Setup stack frame
-	mov rbp, rsp		; Those two lines are equivalent to 'enter'
+	push rbp		; Setup stack frame
+	mov rbp, rsp	; Those two lines are equivalent to 'enter'
 
 	push rdi		; store the arg1 value
 
 	mov rax, 1		; Write syscall
 	mov rdi, 1		; write to stdout
-	mov rsi, rsp		; Address of the character to be printed (Address of the stack)
+	mov rsi, rsp	; Address of the character to be printed (Address of the stack)
 	mov rdx, 1		; Write 1 byte
 	syscall			; call the syscall
 
 	pop rdi			; set back the previos value
 	mov rax, 1		; return value printd
-	mov rsp, rbp		; Restore previous stack frame
+
+	mov rsp, rbp	; Restore previous stack frame
 	pop rbp			; Those two lines are equivalent to 'leave'
-        ret			; return
+
+	ret				; return
