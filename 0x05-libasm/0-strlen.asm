b01e94774543fb05f8685ce0056a6b3e2f5bc2a1
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-09-23 05:14:27 +0000

    Task 0: strlen

--- 0x05-libasm/0-strlen.asm
+++ 0x05-libasm/0-strlen.asm
@@ -4,32 +4,33 @@
 
 	bits 64
 
-	section .text					; Code section
-	global asm_strlen				; Exporting the function name
+	section .text			; Code section
+	global asm_strlen		; Exporting the function name
 
-asm_strlen:							; The symbol or function name label
-	push rbp						; prologue
+asm_strlen:				; The symbol or function name label
+	push rbp			; prologue
 	mov rbp, rsp
 
-	push rbx						; save rbx value in stack
-	push rcx						; save rcx value in stack
+	push rbx			; save rbx value in stack
+	push rcx			; save rcx value in stack
 
-	mov rbx, rdi					; copy the argument(str) in rbx register
-	mov rcx, 0						; rcx register will be the iterator and starts in 0
+	mov rbx, rdi			; copy the argument(str) in rbx register
+	mov rcx, 0			; rcx register will be the iterator and starts in 0
+	jmp entry_i
 
-iter_i:								; loop iter_i ->  while (*str != '\0')
-	inc rcx							; increment the iterator
-	cmp byte [rbx + rcx - 1], 0x00	; compare character of iterator position with caracter null
-	jne iter_i						; if not null jump loop
+iter_i:					; loop iter_i ->  while (*str != '\0')
+	inc rcx				; increment the iterator
+entry_i:
+	cmp byte [rbx + rcx ], 0x00	; compare character of iterator position with caracter null
+	jne iter_i			; if not null jump loop
 
 end:
-	dec rcx							; decrement 1 because of the null character
-	mov rax, rcx					; set the lenght in the return register
+	mov rax, rcx			; set the lenght in the return register
 
-	pop rcx							; restore rcx register from stack
-	pop rbx							; restore rbx register from stack
+	pop rcx				; restore rcx register from stack
+	pop rbx				; restore rbx register from stack
 
-	mov rsp, rbp					; epilogue
+	mov rsp, rbp			; epilogue
 	pop rbp
 
-	ret								; return
+	ret				; return
