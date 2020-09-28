f3a227ed116b7ca258ab63a78ae6efec0c2283aa
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-09-28 23:55:26 +0000

    Task 1: Current handler - signal

--- 0x06-signals/1-current_handler_signal.c
+++ 0x06-signals/1-current_handler_signal.c
@@ -1,5 +1,10 @@
 #include "signals.h"
 
+/**
+ * current_handler_signal - function that retrieves the current handler of the
+ * signal SIGINT
+ * Return - Return the function nandler
+ */
 void (*current_handler_signal(void))(int)
 {
 	void (*handler)(int);
