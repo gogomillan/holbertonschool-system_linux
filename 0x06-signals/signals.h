c99d4038b956f9b99f19292c0046fd7b5b2ee67e
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-09-29 01:11:03 +0000

    Task 3: Current handler - sigaction

--- 0x06-signals/signals.h
+++ 0x06-signals/signals.h
@@ -9,5 +9,6 @@ int handle_signal(void);
 void hdl_ctrl_c(int signal);
 void (*current_handler_signal(void))(int);
 int handle_sigaction(void);
+void (*current_handler_sigaction(void))(int);
 
 #endif  /* gogomillan */
