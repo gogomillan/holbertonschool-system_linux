20f6e3f7e2f7d824c0daaed56c2c5bea29c57da4
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-09-29 04:15:11 +0000

    Task 9: Catch with sh

--- 0x06-signals/9-handle_signal.sh
+++ 0x06-signals/9-handle_signal.sh
@@ -1,7 +1,2 @@
 #!/bin/sh
-# The requirement is for running in sh
-# Using: kill -l
-# SIGABRT(bash) -> ABRT(sh)
-# SIGIO(bash) -> IO(sh)
-# SIGTERM(bash) -> SIGTERM(sh)
 trap "echo Nope" ABRT IO TERM
