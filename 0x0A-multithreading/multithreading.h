deae204c48fb6d4d3cb1712b5cdd8935b59b8735
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-12-23 22:42:01 +0000

    Prime factors

--- 0x0A-multithreading/multithreading.h
+++ 0x0A-multithreading/multithreading.h
@@ -7,6 +7,8 @@
 #include <stdint.h>
 #include <stdarg.h>
 #include <pthread.h>
+#include "list.h"
+#include "pool_threading.h"
 
 /**
  * struct pixel_s - RGB pixel
@@ -75,5 +77,6 @@ int tprintf(char const *format, ...);
 void blur_portion(blur_portion_t const *portion);
 void blur_image(img_t *img_blur, img_t const *img, kernel_t const *kernel);
 int tprintf(char const *format, ...);
+list_t *prime_factors(char const *s);
 
 #endif /*__MULTITHREADING_H__*/
